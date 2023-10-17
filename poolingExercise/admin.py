6fc906416f127391446198ad19e9d9ebdde8004e
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-17 02:10:17 +0200

    Configure admin dashboard

--- poolingExercise/admin.py
+++ poolingExercise/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from poolingExercise.models import *
+
 # Register your models here.
-admin.register(Team)
-admin.register(Person)
+admin.site.register(Team)
+admin.site.register(Person)
--- poolingExercise/models.py
+++ poolingExercise/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
-MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+MONTHS = models.IntegerChoices('Miesiace',
+                               'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
 
 SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+)
 
 
 class Team(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60, null=True)
     country = models.CharField(max_length=2)
-
+    symbol = models.CharField(max_length=365, null=True)
     def __str__(self):
         return f"{self.name}"
 
 
 class Person(models.Model):
-
     name = models.CharField(max_length=60)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default=SHIRT_SIZES[0][0])
     month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0])
