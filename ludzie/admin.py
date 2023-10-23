354b52807aa5f666188b0216a6e5a902b0390a2c
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-23 13:49:12 +0200

    Add osobaAdmin for display in admin panel

--- ludzie/admin.py
+++ ludzie/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from ludzie.models import Osoba, Stanowisko
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ['imie', 'nazwisko', 'data_dodania', 'plec']
 
-admin.site.register(Osoba)
+admin.site.register(Osoba, OsobaAdmin)
 admin.site.register(Stanowisko)
