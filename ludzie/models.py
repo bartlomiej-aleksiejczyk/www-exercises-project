3cce0b42e8128727ef60d6a5ec88e33ff16ed8e6
Bartlomiej Aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-23 13:35:10 +0200

    Repeair models

--- ludzie/models.py
+++ ludzie/models.py
@@ -1,21 +1,27 @@
-import ludzie
 from django.db import models
-
-from ludzie import admin
+import datetime
 
 
 class Stanowisko(models.Model):
-    nazwa = models.CharField(max_length=60, required=True, blank=False)
-    opis = models.CharField(max_length=255, required=False, blank=True)
+    nazwa = models.CharField(max_length=60, null=False, blank=False)
+    opis = models.CharField(max_length=255, null=True, blank=True)
 
 
 class Osoba(models.Model):
-    imie = models.CharField(max_length=60, required=True, blank=False)
-    nazwisko = models.CharField(max_length=60, required=True, blank=False)
-    PLCIE = (
-        ('K', 'Kobieta'),
-        ('M', 'Mezczyzna'),
-        ('I', 'Inna'),
-    )
-    plec = models.CharField(max_length=1, choices=PLCIE)
-    stanowisko = models.ForeignKey(Stanowisko)
+    imie = models.CharField(max_length=60, null=False, blank=False)
+    nazwisko = models.CharField(max_length=60, null=False, blank=False)
+
+    class Plcie(models.IntegerChoices):
+        KOBIETA = 1, "Kobieta"
+        MEZCZYZNA = 2, "Mezczyzna"
+        INNA = 3, "Inna"
+
+    plec = models.IntegerField(choices=Plcie.choices)
+    stanowisko = models.ForeignKey('ludzie.Stanowisko', on_delete=models.CASCADE)
+    data_dodania = models.DateField(default=datetime.date.today, editable=False)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+    class Meta:
+        ordering = ["nazwisko"]
+        verbose_name_plural = "osoby"
